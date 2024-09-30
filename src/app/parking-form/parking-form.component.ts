939f956367e4f798f7ee779e822109bd04500dbc
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-09-30 23:40:12 +0200

    update the parking form to work with service

--- src/app/parking-form/parking-form.component.ts
+++ src/app/parking-form/parking-form.component.ts
@@ -1,30 +1,46 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ParkingSpotService } from '../services/parking-spot.service';
+import { ParkingSpot } from '../services/parking-spot.service'; // Ensure the correct import path
 
 @Component({
   selector: 'app-parking-form',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    CommonModule
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './parking-form.component.html',
-  styleUrl: './parking-form.component.css',
+  styleUrls: ['./parking-form.component.css'],
 })
-export class ParkingFormComponent {
+export class ParkingFormComponent implements OnInit {
+  parkingForm: FormGroup;
+  parkingSpots: ParkingSpot[] = [];
 
-  options = ['option 1', 'option 2', 'option 3']
+  constructor(private parkingSpotService: ParkingSpotService) {
+    this.parkingForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      selectedOption: new FormControl('', [Validators.required]),
+    });
+  }
 
-  parkingForm = new FormGroup({
-    name: new FormControl(''),
-    selectedOption: new FormControl(''),
-  });
+  ngOnInit(): void {
+    // Subscribe to parking spots observable to get the list of spots
+    // use subscribe explicitly
+    this.parkingSpotService.parkingSpotsObservable$.subscribe(spots => {
+      this.parkingSpots = spots;
+    });
+  }
 
+  onSubmit(): void {
+    const { name, selectedOption } = this.parkingForm.value;
+    const selectedSpot = this.parkingSpots.find(spot => spot.id === +selectedOption);
 
+    if (selectedSpot) {
+      this.parkingSpotService.bookSpot(selectedSpot.id, name);
+      this.parkingForm.reset();
+    }
 
-
-  onSubmit(): void{
-
+    if(selectedSpot?.isBooked){
+      alert("that spot is already booked");
+    }
   }
 }
