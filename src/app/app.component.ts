1aec92526e09c1fb0cec9a26cd115908c22d1970
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-09-30 23:39:41 +0200

    add parking grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ParkingFormComponent } from './parking-form/parking-form.component';
+import { ParkingGridComponent } from './parking-grid/parking-grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ParkingFormComponent],
+  imports: [RouterOutlet, ParkingFormComponent, ParkingGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
